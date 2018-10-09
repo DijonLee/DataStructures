0cc511b0859baabbd757510e02f9b8ba493c1102
Jovan-Gianni Lee <jlee251@u.rochester.edu>
2018-10-09 00:16:31 -0400

    Add files via upload

---
+++ Java/Lab4/Main.java
@@ -0,0 +1,153 @@
+
+import java.io.File;
+
+import java.io.FileNotFoundException;
+
+import java.util.Arrays;
+
+import java.util.NoSuchElementException;
+
+import java.util.Scanner;
+
+public class Main {
+
+	static String[] instructionArray = new String[1];
+	static sequenceObj[] sequenceArr;
+	static int arraySize;
+
+	public static void main(String[] args) throws FileNotFoundException {
+		arraySize = Integer.parseInt(args[0]);
+		// Argument 1 = Array Size, Argument 2 = File
+		sequenceArr = new sequenceObj[arraySize]; // Create Sequence Array
+		LList llist = new LList(); // Create linkedlist
+
+		readText(args[0], args[1]);
+		System.out.println("CURRENT SEQUENCE ARRAY");
+
+	}
+
+	/* READ THE COMMAND FILE */
+
+	public static void readText(String arraySize, String filename)
+
+			throws /* FileNotFoundException, */NoSuchElementException, FileNotFoundException {
+
+		Scanner scan = new Scanner(new File(filename)); // scans all the tesxt
+
+
+		while (scan.hasNext()) { // GET INSTRUCTION LINE BY LINE */
+			String line = scan.nextLine(); // go line by line
+			instructionArray = line.split(" ");
+			String[] removedNull = Arrays.stream(instructionArray)
+					.filter(value ->
+					value != null && value.length() > 0
+					)
+					.toArray(size -> new String[size]);
+
+		System.out.println(Arrays.toString(removedNull)); // PRINT COMMANDS
+			commandAction(removedNull); // DETERMINE COMMAND
+
+		}
+
+	}
+ 
+	/* DETERMINE COMMAND */
+	
+	public static void commandAction(String[] commandArr) {
+		if (commandArr.length == 0) { 			// System.out.println("null entry");
+		}
+		/* INSERT OR CLIP */
+		else if (commandArr.length == 4) {
+			if (commandArr[0].equals("insert")) {
+				insertCommand(commandArr);
+			} else if (commandArr[0].equals("clip"))
+				;
+		}
+		/* COPY PRINTPOS */
+		else if (commandArr.length == 3) {
+			if (commandArr[0].equals("copy")) {
+
+				/* TRANSCRIBE, REMOVE OR PRINT POS */
+			} else if (commandArr.length == 2) {
+				if (commandArr[0].equals("transcribe")) {
+
+				} else if (commandArr[0].equals("remove")) {
+					removeCommand(commandArr);
+
+				}
+			} else if (commandArr[0].equals("print")) {
+				printPosCommand(commandArr);
+				
+					
+				
+			}
+		}
+
+		else if (commandArr.length == 1) {
+			if (commandArr[0].equals("print")) {
+				for (int i = 0; i < arraySize; i++) {
+					if (sequenceArr[i] != null) {
+						System.out.println(sequenceArr[i]);
+
+					}
+
+					// System.out.println(Arrays.toString(sequenceArr));
+				}
+			}
+		}
+	}
+
+	public static void insertCommand(String[] commandArr) {
+		// [insert, 2, DNA, AGG]
+		// Take in Position, Type, Sequence
+		sequenceObj myObj = new sequenceObj(commandArr[1], commandArr[2], commandArr[3]);
+		int position = (Integer.parseInt(commandArr[1]) - 1);
+		// System.out.println("" + myObj.toString());
+		 myObj.printSequence();
+
+		// INSERT INTO ARRAY POSITION
+		sequenceArr[position] = myObj;
+
+	}
+
+	public static void transcribeCommand(String[] commandArr) {
+
+	}
+
+	public static void printPosCommand(String[] commandArr) {
+		int position = Integer.parseInt(commandArr[1]);
+		
+		System.out.println(commandArr[position]); // PRINT COMMANDS
+
+/*
+		// CREATE A LINKEDLIST SEQUENCE
+		String[] sequenceLink = takeSequence.split("");
+		for (int i = 0; i < sequenceLink.length; i++) {
+			mySequence.insert(sequenceLink[i]);
+
+		}
+		*/
+
+	}
+	// REMOVE POS
+	public static void removeCommand(String[] commandArr) {
+		int position = Integer.parseInt(commandArr[1]);
+		sequenceArr[position] = null;
+
+	}
+
+	public static void copyCommand() {
+
+	}
+
+	public static void clipCommand() {
+
+	}
+
+	public static void printCommand(sequenceObj[] sequenceArr, int arraySize) {
+
+	}
+
+}
+
+// Updated
---
+++ Java/Lab4/sequenceObj.java
@@ -0,0 +1,51 @@
+
+enum type {
+	DNA, RNA;
+}
+
+public class sequenceObj {
+	type myType;
+	int position;
+	LList<String> mySequence = new LList<String>();
+
+	public sequenceObj(String takeInPosition, String takeType, String takeSequence) {
+		// SET DNA OR RNA
+		if (takeType.equals("DNA")) {
+			myType = type.DNA;
+
+		} else {
+			myType = type.RNA;
+
+		}
+		// SET POSITION
+		position = Integer.parseInt(takeInPosition);
+
+		// CREATE A LINKEDLIST SEQUENCE
+		String[] sequenceLink = takeSequence.split("");
+		for (int i = 0; i < sequenceLink.length; i++) {
+			// System.out.println(sequenceLink[i]);
+			mySequence.insert(sequenceLink[i]);
+			//System.out.println(sequenceLink[i]);
+
+		}
+	//	System.out.println("INNER SEQUENCE");
+	//	mySequence.print();
+
+	}
+
+	@Override
+	public String toString() {
+		// mySequence.print();
+		return ("\n  type:" + myType + "    position: " + position  + " List:" +mySequence+ " \n");
+
+	}
+
+	public void printSequence() {
+		System.out.println("PRINTING MY SEQUENCE");
+		mySequence.print();
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
