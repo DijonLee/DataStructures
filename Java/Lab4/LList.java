dcdd32fa8ad377ed2d675a4273948f0fc06d5c0e
Jovan-Gianni Lee <jlee251@u.rochester.edu>
2018-10-09 19:08:54 -0400

    Update LList.java

--- Java/Lab4/LList.java
+++ Java/Lab4/LList.java
@@ -1,263 +1,36 @@
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+public class Link<E> {
+	private E element; // Value for this node
+	private Link<E> next; // Pointer to next node in list
+	// Constructors
 
-/** Linked list implementation */
-public class LList<E> implements List<E> {
-	private Link<E> head; // Pointer to list header
-	private Link<E> tail; // Pointer to last element
-	protected Link<E> curr; // Access to current element
-	private int cnt; // Size of list
-
-	/** Constructors */
-	LList(int size) {
-		this();
-	} // Constructor -- Ignore size
-
-	LList() {
-		curr = tail = head = new Link<E>(null); // Create header
-		cnt = 0;
+	Link(E it, Link<E> nextval) {
+		element = it;
+		next = nextval;
 	}
 
-	/** Remove all elements */
-	public void clear() {
-		head.setNext(null); // Drop access to links
-		curr = tail = head = new Link<E>(null); // Create header
-		cnt = 0;
+	Link(Link<E> nextval) {
+		next = nextval;
 	}
 
-	/** Insert "it" at current position */
-	public void insert(E it) {
-		curr.setNext(new Link<E>(it, curr.next()));
-		if (tail == curr)
-			tail = curr.next(); // New tail
-		cnt++;
+	Link<E> next() {
+		return next;
+	} // Return next field
+
+	Link<E> setNext(Link<E> nextval) // Set next field
+	{
+		return next = nextval;
+	} // Return element field
+
+	E element() {
+		return element;
+	} // Set element field
+
+	E setElement(E it) {
+		return element = it;
 	}
 
-	/** Append "it" to list */
-	public void append(E it) {
-		tail = tail.setNext(new Link<E>(it, null));
-		cnt++;
-	}
-
-	/** Remove and return current element */
-	public E remove() {
-		if (curr.next() == null)
-			return null; // Nothing to remove
-		E it = curr.next().element(); // Remember value
-		if (tail == curr.next())
-			tail = curr; // Removed last
-		curr.setNext(curr.next().next()); // Remove from list
-		cnt--; // Decrement count
-		return it; // Return value
-	}
-
-	/** Set curr at list start */
-	public void moveToStart() {
-		curr = head;
-	}
-
-	/** Set curr at list end */
-	public void moveToEnd() {
-		curr = tail;
-	}
-
-	public void print() {
-		String result = "";
-		while (curr.next() != null) {
-			curr = curr.next();
-			result += curr.element() + ",";
-		}
-		System.out.print(result);
-
-	}
-
-	public String getasString() {
-		String result = "";
-		while (curr.next() != null) {
-			curr = curr.next();
-			result += curr.element() + ",";
-		}
-		return result;
-
-	}
-
-	/** Move curr one step left; no change if now at front */
-	public void prev() {
-		if (curr == head)
-			return; // No previous element
-		Link<E> temp = head;
-		// March down list until we find the previous element
-		while (temp.next() != curr)
-			temp = temp.next();
-		curr = temp;
-	}
-
-	/** Move curr one step right; no change if now at end */
-	public void next() {
-		if (curr != tail)
-			curr = curr.next();
-	}
-
-	/** @return List length */
-	public int length() {
-		return cnt;
-	}
-
-	/** @return The position of the current element */
-	public int currPos() {
-		Link<E> temp = head;
-		int i;
-		for (i = 0; curr != temp; i++)
-			temp = temp.next();
-		return i;
-	}
-
-	/** Move down list to "pos" position */
-	public void moveToPos(int pos) {
-		assert (pos >= 0) && (pos <= cnt) : "Position out of range";
-		curr = head;
-		for (int i = 0; i < pos; i++)
-			curr = curr.next();
-	}
-
-	/** @return Current element value */
-	public E getValue() {
-		if (curr.next() == null)
-			return null;
-		return curr.next().element();
-	}
-
-	@Override
-	public boolean add(E e) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void add(int index, E element) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean addAll(Collection<? extends E> c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean addAll(int index, Collection<? extends E> c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean contains(Object o) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean containsAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public E get(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int indexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int lastIndexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public ListIterator<E> listIterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ListIterator<E> listIterator(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean remove(Object o) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public E remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean removeAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean retainAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public E set(int index, E element) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public List<E> subList(int fromIndex, int toIndex) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T> T[] toArray(T[] a) {
+	public char[] getValue() {
 		// TODO Auto-generated method stub
 		return null;
 	}
