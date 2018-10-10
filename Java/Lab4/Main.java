e91800b50689939ffb89a5dbe9d757f9efa2cf3b
Jovan-Gianni Lee <jlee251@u.rochester.edu>
2018-10-09 21:36:29 -0400

    Update Main.java

--- Java/Lab4/Main.java
+++ Java/Lab4/Main.java
@@ -171,8 +171,8 @@ public class Main {
 	}
 
 	public static void printPosCommand(String[] commandArr) {
-		// int position = Integer.parseInt(commandArr[1]);
-		// System.out.println(sequenceArr[position].mySequence.toString());
+		 int position = Integer.parseInt(commandArr[1]);
+		 System.out.println(sequenceArr[position].mySequence.toString());
 
 		// sequenceArr[position].mySequence.print();
 		// System.out.println("Print Position" + commandArr[position]); // PRINT
