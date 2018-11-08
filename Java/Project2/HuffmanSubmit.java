1e930e68deb43d12ee1506bb6b57b468fa9a70bd
Jovan-Gianni Lee <jlee251@u.rochester.edu>
2018-11-07 20:31:59 -0500

    Update HuffmanSubmit.java

--- Java/Project2/HuffmanSubmit.java
+++ Java/Project2/HuffmanSubmit.java
@@ -1,347 +1,216 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.Writer;
-import java.nio.charset.Charset;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.PriorityQueue;
-import java.util.Properties;
-import java.util.Scanner;
-import java.util.Set;
-
-// Import any package as required
 
 public class HuffmanSubmit implements Huffman {
-	/* Global Variables */
-	static int frequencyListSize;
-	static Map<Character, Integer> frequencies = new HashMap<>();
-	static Map<Character, String> encodedfreqs = new HashMap<>();
-     static Charset ASCII = Charset.forName("ASCII");
-
-	static String hEncodeInputFile;
-	static String hDecodeFile;
-	static String hDecodeFileOut;
-
-
-	Properties prop = new Properties();
-
-	static Map<String, String> map = new HashMap();
-
-	static PriorityQueue<Node> queue = new PriorityQueue<Node>();
-
-	/* Node Class */
-	static class Node {
-		int data;
-		char c;
-		Node left;
-		Node right;
-	}
-
-	/* Compare Nodes */
-	static class MyComparator implements Comparator<Node> {
-		public int compare(Node x, Node y) {
-
-			return x.data - y.data;
-		}
-	}
 
-	public static void printTree(Node root, String s) throws IOException {
-		if (root.left == null && root.right == null) {
-			// System.out.println(root.c + ":" + s); // Prints freq chart
-
-			if ((root.c == '\n')) {
-				encodedfreqs.put('\n', s);
-			} else {
-				encodedfreqs.put(root.c, s);
-			}
-			return;
-		}
-		printTree(root.left, s + "0");
-		printTree(root.right, s + "1");
-		// System.out.println(Collections.singletonList(encodedfreqs));
-
-	}
-
-	public void encode(String inputFile, String outputFile, String freqFile) {
-		hEncodeInputFile = inputFile;
-		try {
-			countFrequency();
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try {
-			makeHeap();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	public static void main(String[] args) throws IOException {
-
-		Huffman huffman = new HuffmanSubmit();
-
-		//huffman.encode("ur.jpg", "ur.enc", "freq.txt");
-		huffman.decode("ur.enc", "ur_dec.jpg", "freq.txt");
-	}
-
-	public static void countFrequency() throws IOException {
-
-		FileReader inputStream = null;
-		PrintWriter outputStream = null;
-
-		try {
-			inputStream = new FileReader(hEncodeInputFile);
-			outputStream = new PrintWriter("tester.txt");
-
-			int c;
-			while ((c = inputStream.read()) != -1) {
-				frequencies.put(((char) c), frequencies.getOrDefault(((char) c), 0) + 1);
-				outputStream.write((char) c); // 146kb here (kept whitespace)
-				//System.out.print(Collections.singletonList(frequencies));
-			}
-
-		} finally {
-			if (inputStream != null) {
-				inputStream.close();
-
-			}
-			if (outputStream != null) {
-				outputStream.close();
-			}
-		}
-		frequencyListSize = frequencies.size();
-	}
-
-	private static void counttoEncode() throws IOException {
-		FileReader inputStream = null;
-		FileReader inputStream2 = null;
-
-		PrintWriter outputStream = null;
-		PrintWriter outputStream2 = null;
-
-		/* Writes encoded file */
-		try {
-			inputStream = new FileReader(hEncodeInputFile);
-			outputStream2 = new PrintWriter("mytest.txt"); // 146kb here (kept whitespace)
-
-			outputStream = new PrintWriter("ur.enc");
-			inputStream2 = new FileReader("ur.enc");
-
-			int c;
-
-			while ((c = inputStream.read()) != -1) {
-				char cToChar = (char) c;
-
-				if (encodedfreqs.containsKey(cToChar)) {
-					outputStream.write(encodedfreqs.get(cToChar));
-
-				}
-
-				outputStream2.write(c);
-
-			}
-			outputStream2.close();
-			outputStream.close();
-
-		} finally
-
-		{
-
-			if (inputStream != null) {
-
-				inputStream.close();
-
-			}
-			if (outputStream != null) {
-				outputStream.close();
-				// }
-			}
-
-			int c;
-
-		}
-
-		PrintWriter writer = new PrintWriter("freq.txt", "ASCII");
-		
-
-		Iterator it = frequencies.entrySet().iterator();
-		Iterator it2 = encodedfreqs.entrySet().iterator();
-
-		while (it.hasNext()) {
-			Map.Entry pair = (Map.Entry) it.next();
-			Map.Entry pair2 = (Map.Entry) it2.next();
-
-			writer.println(pair2.getKey() + "=" + pair2.getValue());
-		}
-		writer.close();
-
-	}
-
-	public static void makeHeap() throws IOException {
-
-		queue = new PriorityQueue<Node>(frequencyListSize, new MyComparator());
-
-		for (Entry<Character, Integer> entry : frequencies.entrySet()) {
-			Node myNode = new Node();
-			Character key = entry.getKey();
-			Object value = entry.getValue();
-
-			myNode.c = key;
-			myNode.data = (int) value;
-
-			myNode.left = null;
-			myNode.right = null;
-			queue.add(myNode);
-
-		}
-		Node root = null;
-
-		while (queue.size() > 1) {
-			Node x = queue.peek();
-			queue.poll();
-
-			// second min extarct.
-			Node y = queue.peek();
-			queue.poll();
-
-			// new node f which is equal
-			Node f = new Node();
-
-			// to the sum of the frequency of the two nodes
-			// assigning values to the f node.
-			f.data = x.data + y.data;
-			f.c = '-';
-
-			// first extracted node as left child.
-			f.left = x;
-
-			// second extracted node as the right child.
-			f.right = y;
-
-			// marking the f node as the root node.
-			root = f;
-
-			// add this node to the priority-queue.
-			queue.add(f);
-		}
-
-		// print the codes by traversing the tree
-
-		printTree(root, "");
-		counttoEncode();
-
-	}
-
-	/* DECODE */
-	public void decode(String inputFile, String outputFile, String freqFile) {
-		// huffman.decode("ur.enc", "ur_dec.jpg", "");
-		
-
-		FileReader inputStream = null;
-		FileReader inputFreq = null;
-
-		FileWriter outputStream = null;
-
-		try {
-			prop.load(new FileReader(freqFile));
-			for (Map.Entry entry : prop.entrySet()) { // map.put((String) entry.getKey(), (String) entry.getValue());
-
-			}
-			BufferedReader in = new BufferedReader(new FileReader(freqFile));
-
-			String line;
-			while ((line = in.readLine()) != null) {
-				if (line.contains("=")) {
-					System.out.println(line);
-					String[] strings = line.split("=");
-					map.put(strings[0], (strings[1]));
-				}
-			}
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		hDecodeFile = inputFile;
-		hDecodeFileOut = outputFile;
-		
-		try {
-			DecodeNext();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void DecodeNext() throws IOException {
-
-		FileReader inputStream = null;
-		inputStream = new FileReader(hDecodeFile);
-		//FileWriter outputStream = null;
-		//outputStream = new FileWriter(hDecodeFileOut);
-		
-		File file = new File(hDecodeFile);
-		BufferedWriter outputStream = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), ASCII));
-		int c;
-		String decoder = "";
-
-		Iterator it = map.entrySet().iterator();
-		while (it.hasNext()) { // System.out.println(Collections.singletonList(map));
-
-			Map.Entry pair = (Map.Entry) it.next();
-		}
-		String key = "";
-
-		while ((c = inputStream.read()) != -1) {
-			char cToChar = (char) c;
-			decoder += cToChar;
-			if (map.containsValue(decoder)) {
-				for (Map.Entry entry : map.entrySet()) {
-					if (decoder.equals(entry.getValue())) {
-						key = (String) entry.getKey();
-	//					 System.out.println("Value" + entry.getValue());
-						 
-//						 System.out.print(" Key" + entry.getKey());
-
-						System.out.println(entry.getKey());
-						if (entry.getKey().equals("")) { // Where is my problem???
-							outputStream.write("\n");
-							decoder = "";
-						//	outputStream.write(key);
-						}
-
-						else {
-							decoder = "";
-							outputStream.write(key);
-
-						}
-						break; // breaking because its one to one map }
-					}
-				}
-
-			}
-
-		}
-		outputStream.close();
-
-	}
+ static HashMap < Character, Integer > numOfChar = new HashMap < Character, Integer > ();
+ static HashMap < Character, String > encoding = new HashMap < Character, String > ();
+ static PriorityQueue < Node > pq2 = new PriorityQueue < Node > ();
+ static PriorityQueue < Node > pq = new PriorityQueue < Node > ();
+ HashMap < Character, Integer > decode = new HashMap < Character, Integer > ();
+
+ public void encode(String inputFile, String outputFile, String freqFile) {
+  BinaryIn bin = new BinaryIn(inputFile);
+
+  // filling hashmap with quantity of each char
+
+  while (bin.isEmpty() == !true) {
+   char c2 = bin.readChar();
+
+   if (numOfChar.containsKey(c2)) {
+    numOfChar.put(c2, numOfChar.get(c2) + 1);
+   } else {
+    numOfChar.put(c2, 1);
+   }
+
+  }
+
+  // System.out.println(numOfChar);
+
+  // creating tree using priority queue
+  // Fixed
+  for (Entry < Character, Integer > item: numOfChar.entrySet()) {
+   Character key = item.getKey();
+   Integer value = item.getValue();
+
+   char c = item.getKey();
+   Node newLeaf = new Node(numOfChar.get(c), c);
+   pq.add(newLeaf);
+  }
+
+  while ((pq.size() <= 1) == false) {
+   Node smallest = pq.remove();
+   Node secondSmallest = pq.remove();
+   Node newNode = new Node(smallest, secondSmallest);
+   pq.add(newNode);
+  }
+
+  Node root = pq.remove();
+
+  traverseTree(root, encoding, "");
+
+  // System.out.println(encoding);
+
+  // writing to output file
+  BinaryOut bout = new BinaryOut(outputFile);
+  BinaryIn bin2 = new BinaryIn(inputFile);
+  while (!bin2.isEmpty()) {
+   char c = bin2.readChar();
+   String str = encoding.get(c);
+   for (int i = 0; i < str.length(); i++) {
+
+    char binary = str.charAt(i);
+    if (binary != '0') {
+     bout.write(true);
+     // bout.flush();
+    } else {
+     bout.write(false);
+     // bout.flush();
+    }
+   }
+   // bout.flush();
+  }
+  bout.close();
+  // writing to freq file
+  try {
+   PrintWriter writer = new PrintWriter(freqFile);
+
+   for (Character c: numOfChar.keySet()) {
+    String str = Integer.toBinaryString(c);
+    int freq = numOfChar.get(c);
+    writer.println(str + ":" + freq);
+   }
+   writer.close();
+  } catch (FileNotFoundException e) {
+   e.printStackTrace();
+  }
+
+ }
+
+ public static void traverseTree(Node root, HashMap < Character, String > encoding, String binary)
+ throws NullPointerException {
+  try {
+
+   if (!(root.leftChild == (null))) {
+    traverseTree(root.leftChild, encoding, binary + "0");
+   }
+   if (!(root.rightChild == null)) {
+    traverseTree(root.rightChild, encoding, binary + "1");
+   }
+   if (root.leftChild == null && root.rightChild == null) {
+    encoding.put(root.character, binary);
+   }
+  } finally {
+
+  }
+ }
+
+ public void decode(String inputFile, String outputFile, String freqFile) {
+  try {
+   Scanner sc = new Scanner(new File("freq.txt"));
+   while (sc.hasNext() != false) {
+    String line = sc.nextLine();
+    String[] binary = line.split(":");
+    char c = (char) Integer.parseInt(binary[0], 2); // parsing string as a binary number
+    decode.put(c, Integer.parseInt(binary[1]));
+   }
+
+   // creating tree using priority queue
+
+   Iterator < Character > it = decode.keySet().iterator();
+   while (it.hasNext()) {
+    char c = it.next();
+    Node newLeaf = new Node(decode.get(c), c);
+    pq2.add(newLeaf);
+   }
+
+   while (!(pq2.size() <= 1) == true) {
+    Node smallest = pq2.remove();
+    Node secondSmallest = pq2.remove();
+    Node newNode = new Node(smallest, secondSmallest);
+    pq2.add(newNode);
+   }
+
+   Node root = pq2.remove();
+
+   // writing to output file
+   BinaryOut bout = new BinaryOut(outputFile);
+   BinaryIn bin = new BinaryIn(inputFile);
+
+   Node node = root;
+
+   while (bin.isEmpty() == false) {
+
+    boolean move = bin.readBoolean();
+    if (node.leftChild == null && node.rightChild == null) {
+     bout.write(node.character);
+     node = root;
+    }
+    if (move) {
+     node = node.rightChild;
+    } else {
+     node = node.leftChild;
+    }
+
+   }
+   bout.close();
+
+  } catch (FileNotFoundException e) {
+   e.printStackTrace();
+  }
+ }
+
+ public static void main(String[] args) {
+
+  Huffman huffman = new HuffmanSubmit();
+
+  huffman.encode(args[0], "ur.enc", "freq.txt");
+  huffman.decode("ur.enc", "ur.decode", "freq.txt");
+
+ }
+
+}
+
+class Node implements Comparable {
+ int freq;
+ char character;
+ Node leftChild;
+ Node rightChild;
+
+ public Node(int val, char character) {
+  this.freq = val;
+  this.character = character;
+  leftChild = null;
+  rightChild = null;
+ }
+
+ public Node(Node x, Node y) {
+  this.freq = x.freq + y.freq;
+  this.character = '-';
+
+  if (x.freq < y.freq) {
+   leftChild = x;
+
+  } else {
+   leftChild = y;
+
+  }
+  if (x.freq >= y.freq) {
+   rightChild = x;
+  } else {
+   rightChild = y;
+
+  }
+
+ }
+
+ public int compareTo(Object o) {
+  Node o2 = (Node) o;
+  if ((o2.freq <= this.freq) == false) {
+   return -1;
+  } else {
+   return 1;
+  }
+ }
 }
