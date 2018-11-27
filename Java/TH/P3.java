4860e0f18532112919470d82f19f3cdab9d3afa1
Jovan-Gianni Lee <jlee251@u.rochester.edu>
2018-11-27 18:47:40 -0500

    Update P3.java

--- Java/TH/P3.java
+++ Java/TH/P3.java
@@ -3,7 +3,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 
-
 //row l-r
 //col u-d
 public class P3 {
@@ -12,13 +11,17 @@ public class P3 {
 		int A = 0; // row
 		int B = 0; // col
 		String[] graph = null;
-/* Text file to variables */
+		String[][] myGraph = null;
+
+		/* Text file to variables */
 		try {
 			boolean getFirst = true;
 			String[] words;
 
 			reader = new BufferedReader(new FileReader("p3_in1"));
 			String line = reader.readLine();
+			int col = 0;
+
 			while (line != null) {
 
 				if (getFirst) {
@@ -27,12 +30,17 @@ public class P3 {
 					getFirst = false;
 					A = Integer.parseInt(words[0]);
 					B = Integer.parseInt(words[1]);
-
+					myGraph = new String[A][B];
 				} else {
 					// System.out.println(line);
 					graph = line.split("");
-					System.out.println(Arrays.toString(graph));
+					for (int i = 0; i < graph.length; i++) {
+					//	System.out.println(graph[i]);
+						myGraph[col][i] = graph[i];
+					}
 
+					System.out.println(Arrays.toString(graph));
+					col++;
 				}
 
 				line = reader.readLine();
@@ -42,19 +50,34 @@ public class P3 {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		Solution(A,B,graph);
-		
+		Solution(A, B, graph, myGraph);
+
 	}
-	
-	public static int Solution(int a, int b, String[] graph) {
-// Determine exits
-		System.out.println(Arrays.toString(graph));
 
-		for (int i=0;i<=a;i++) {
-			System.out.println(graph[i]);
+	public static int Solution(int a, int b, String[] graph, String[][] myGraph) {
+
+		// Determine exits
+
+		// System.out.println("sep");
+	//	System.out.println(Arrays.toString(graph));
+		System.out.println("--");
+
+		System.out.println(Arrays.toString(myGraph[0]));
+		System.out.println(Arrays.toString(myGraph[1]));
+		System.out.println(Arrays.toString(myGraph[2]));
+
+		for (int i = 0; i <= a; i++) {
+			for (int j = 0; j < graph.length; j++) {
+				// System.out.println(String.valueOf(graph[i].charAt(0)));
+				// System.out.println(j);
+
+				// System.out.println(graph[i]);
+
+			}
+
 		}
-			
 
-		return 0 ;
+		return 0;
+
 	}
 }
