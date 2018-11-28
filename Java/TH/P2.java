befad58984d0bf1ccffd2228cf62f604d9487385
Jovan-Gianni Lee <jlee251@u.rochester.edu>
2018-11-28 03:13:08 -0500

    Update P2.java

--- Java/TH/P2.java
+++ Java/TH/P2.java
@@ -1,192 +1,149 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
+// CPP code for printing shortest path between 
+// two vertices of unweighted graph 
 public class P2 {
-
-	public static class Graph {
-		int V;
-		LinkedList<Integer> adjListArray[];
-
-		Graph(int V) {
-			this.V = V;
-			adjListArray = new LinkedList[V];
-			for (int i = 0; i < V; i++) {
-				adjListArray[i] = new LinkedList<>();
-			}
-		}
-
-		public boolean isNeighbor(int u, int v) {
-
-			if (adjListArray[u] == null)
-				return false;
-			return adjListArray[u].contains(v);
-
-		}
+	// utility function to form edge between two vertices
+	// source and dest
+	public static void add_edge(ArrayList<Integer> adj, int src, int dest) {
+		adj.add(src,dest);
+		adj.add(dest,src);
 	}
 
-	public static void addEdges(Graph graph, int src, int dest) {
-		graph.adjListArray[src].add(dest);
+	// a modified version of BFS that stores predecessor
+	// of each vertex in array p
+	// and its distance from source in array d
+	public static boolean BFS(ArrayList<Integer> adj, int src, int dest, int v, int[] pred, int[] dist) {
+		// a queue to maintain queue of vertices whose
+		// adjacency list is to be scanned as per normal
+		// DFS algorithm
+		LinkedList<Integer> queue = new LinkedList<Integer>();
 
-		// Since graph is undirected, add an edge from dest
-		// to src also
-		graph.adjListArray[dest].add(src);
-	}
+		// boolean array visited[] which stores the
+		// information whether ith vertex is reached
+		// at least once in the Breadth first search
+		boolean[] visited = new boolean[v];
 
-	public static void printGraph(Graph graph) {
-		for (int v = 0; v < graph.V; v++) {
-			System.out.println("Adjacency list of vertex " + v);
-			System.out.print("head");
-			for (Integer pCrawl : graph.adjListArray[v]) {
-				System.out.print(" -> " + pCrawl);
-			}
-			System.out.println("\n");
+		// initially all vertices are unvisited
+		// so v[i] for all i is false
+		// and as no path is yet constructed
+		// dist[i] for all i set to infinity
+		for (int i = 0; i < v; i++) {
+			visited[i] = false;
+			dist[i] = Integer.MAX_VALUE;
+			pred[i] = -1;
 		}
-	}
 
-	public static ArrayList<Integer> BFS(Graph graph, int src, int dest) {
-		ArrayList<Integer> shortestPath = new ArrayList<Integer>();
-		boolean[] visited = new boolean[graph.V];
-		if (src == dest) {
-			return null;
-
-		}
-		Queue<Integer> q = new LinkedList();
-		Stack<Integer> paths = new Stack<Integer>();
-
-		q.add(src);
-		paths.add(src);
+		// now source is first to be visited and
+		// distance from source to itself should be 0
 		visited[src] = true;
+		dist[src] = 0;
+		queue.addLast(src);
 
-		while (!q.isEmpty()) {
-			int u = q.poll();
-			for (Integer pCrawl : graph.adjListArray[u]) {
-				if (visited[pCrawl] == false) {
-					q.add(pCrawl);
-					visited[pCrawl] = true;
-					paths.add(pCrawl);
-					System.out.println(u + " " + dest);
+		// standard BFS algorithm
+		while (!queue.isEmpty()) {
+			int u = queue.poll();
+		//	queue.removeFirst();
+			for (int i = 0; i < adj.size(); i++) {
+				if (visited[adj.get(i)] == false) {
+					visited[adj.get(i)] = true;
+					dist[adj.get(u)] = dist[u] + 1;
+					pred[adj.get(u)] = u;
+					queue.addLast(adj.get(u));
 
-					if (u == dest)
-						break;
+					// We stop BFS when we find
+					// destination.
+					if (adj.get(u) == dest) {
+						return true;
+					}
 				}
-
-			}
-		}
-		int node = dest;
-		int currentSrc = dest;
-		shortestPath.add(dest);
-
-		while (!paths.isEmpty()) {
-			System.out.println("got here2");
-
-			node = paths.pop();
-			if (graph.isNeighbor(currentSrc, node)) {
-				shortestPath.add(node);
-				currentSrc = node;
-				System.out.println("got here2222");
-
-				if (node == src)
-					System.out.println("got here");
-
-					System.out.println(node + " " + src);
-
-					break;
 			}
 		}
 
-		return shortestPath;
+		return false;
+	}
+
+	// utility function to print the shortest distance
+	// between source vertex and destination vertex
+	public static void printShortestDistance(ArrayList<Integer> adj, int s, int dest, int v) {
+		// predecessor[i] array stores predecessor of
+		// i and distance array stores distance of i
+		// from s
+		int[] pred = new int[v];
+		int[] dist = new int[v];
+
+		if (BFS(adj, s, dest, v, pred, dist) == false) {
+			System.out.print("Given source and destination");
+			System.out.print(" are not connected");
+			return;
+		}
+
+		// vector path stores the shortest path
+		ArrayList<Integer> path = new ArrayList<Integer>();
+		int crawl = dest;
+		path.add(crawl);
+		while (pred[crawl] != -1) {
+			path.add(pred[crawl]);
+			crawl = pred[crawl];
+		}
+
+		// distance from source is in distance array
+		System.out.print("Shortest path length is : ");
+		System.out.print(dist[dest]);
+
+		// printing path from source to destination
+		System.out.print("\nPath is::\n");
+		for (int i = path.size() - 1; i >= 0; i--) {
+			System.out.print(path.get(i));
+			System.out.print(" ");
+		}
 	}
 
 	public static void main(String[] args) {
+		int v = 100;   // n -1
+		int source = 1;
+		int dest = 8;
+		ArrayList<Integer> adj = new ArrayList<Integer>(v);
 
-		// HashMap <Integer,Integer><Intger> map = new HashMap();
-
-		int maxNum = 0;
-		String matrix[] = null;
-		int[][] realMatrix = null;
-		BufferedReader reader;
-
-		// Construct Initial Matrix O(n)
-		try {
-			boolean getFirst = true;
-			String[] words;
-			reader = new BufferedReader(new FileReader("p2_in1"));
-			String line = reader.readLine();
-			int col = 0;
-
-			while (line != null) {
-				// System.out.println(line);
-				matrix = line.split(" ");
-				if (Integer.parseInt(matrix[0]) > maxNum) {
-					maxNum = Integer.parseInt(matrix[0]);
-				}
-				if (Integer.parseInt(matrix[1]) > maxNum) {
-					maxNum = Integer.parseInt(matrix[1]);
-				}
-				line = reader.readLine();
-				System.out.println(Arrays.toString(matrix));
+		// Creating graph given in the above diagram. 
+		// add_edge function takes adjacency list, source  
+		// and destination vertex as argument and forms 
+		// an edge between them. 
+		
+		for (int i = 0; i < 10; i++) {
+			  adj.add(0);
 			}
+		add_edge(adj, 8, 9);
+		add_edge(adj, 1, 8);
+		add_edge(adj, 1, 2);
+		add_edge(adj, 1, 3);
+		add_edge(adj, 2, 4);
+		add_edge(adj, 4, 3);
+		add_edge(adj, 5, 4);
+		add_edge(adj, 5, 6);
+		add_edge(adj, 6, 8);
+		add_edge(adj, 8, 7);
+		add_edge(adj, 7, 5);
+				
+		printShortestDistance(adj, source, dest, v);
+	}
+/*
+ * 
+ * 8 9
+1 8
+1 2
+1 3
+2 4
+4 3
+5 4
+5 6
+6 8
+8 7
+7 5
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		realMatrix = new int[maxNum + 1][maxNum + 1];
-		int V = maxNum + 1;
-		Graph graph = new Graph(V);
-
-		System.out.println("------------------------");
-		BufferedReader reader2;
-
-		// Construct Initial Matrix O
-		try {
-			boolean getFirst = true;
-			String[] words;
-			reader2 = new BufferedReader(new FileReader("p2_in1"));
-			String line = reader2.readLine();
-			int col = 0;
-
-			while (line != null) {
-				matrix = line.split(" ");
-				System.out.println(matrix[0] + " " + matrix[1]);
-				int mySrc = Integer.parseInt(matrix[0]);
-				int myDest = Integer.parseInt(matrix[1]);
-
-				addEdges(graph, mySrc, myDest);
-				// realMatrix[Integer.parseInt(matrix[0])][Integer.parseInt(matrix[1])] = 1;
-				line = reader2.readLine();
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		// getShortestPath(graph, S, T);
-		// Dont delete
-		LinkedList<Integer> adjListArray[];
-		adjListArray = new LinkedList[maxNum];
-		//
-		printGraph(graph);
-		System.out.println();
-
-		printShortestDistance(graph, 7, 4);
-
+ */
 	}
 
-	public static void printShortestDistance(Graph graph, int src, int dest) {
+//
 
-		ArrayList<Integer> path = BFS(graph, src, dest);
 
-		for (int node : path) {
-			System.out.print(node + " ");
-		}
-
-	}
-
-}
